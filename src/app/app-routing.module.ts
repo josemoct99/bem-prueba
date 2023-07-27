d7d3c5dcd3910e4aecc20b7778668d9619ffcf46
Jose Ricaurte <jose.ricaurte99@gmail.com>
2023-07-26 19:58:18 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookSearchComponent } from './book-search/book-search.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: BookListComponent },
+  { path: 'search', component: BookSearchComponent },
+  { path: 'create', component: CreateBookComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookSearchComponent } from './book-search/book-search.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookSearchComponent,
+    CreateBookComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
